6f602c21179e21410abb97271cf0a39d687643da
robo <podobar@student.mini.pw.edu.pl>
2019-06-04 20:08:21 +0200

    Calendar section introduced, will be implemented soon.

--- sandal_app/lib/tempSubMenu.dart
+++ sandal_app/lib/tempSubMenu.dart
@@ -48,7 +48,6 @@ class _CommentsListedPageState extends State<CommentsSectionPage>{
               ),
             ]),
     onTap: () =>{
-      //TODO: Uciszyć albo co
       Navigator.push((context), MaterialPageRoute(builder: (context) => _CommentDetailsPage(document['title'],document['author'],document['body'])))
     },);
   }
